b571ecfef4c99340074d271d82b4eb5f3ee2b26f
Jess <jesslee1071@gmail.com>
2021-04-16 01:12:46 -0700

    nodejs

--- nodejs/value.js
+++ nodejs/value.js
@@ -1,5 +1,5 @@
 var redis = require('redis');
-var client = redis.createClient(port, host); 
+var client = redis.createClient(12282, process.env.HOST); 
 
 client.on('connect', function() {
     console.log('connected');
