817ad369da22f3ff5609a1c817e58f16af370480
jpasqui <jeremypasquino@gmail.com>
2021-04-18 21:30:55 -0400

    basic car movement

--- unity project/Assets/Scripts/PlayerMovement.cs
+++ unity project/Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    float moveSpeed = 0f;
+    float accSpeed = .1f;
+    float rotSpeed = 100f;
+    float minSpeed = -5f;
+    float maxSpeed = 10f;
 
     public Rigidbody2D rb;
-    Vector2 movement;
+    float movement;
+    float rotation;
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        rotation = Input.GetAxisRaw("Horizontal");
+        movement = Input.GetAxisRaw("Vertical");
     }
 
     void FixedUpdate() {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        moveSpeed = Mathf.Clamp(accSpeed * movement + moveSpeed, minSpeed, maxSpeed);
+        if (movement == 0) {
+            moveSpeed -= (moveSpeed >= 0 ? accSpeed : -accSpeed);
+            moveSpeed = (moveSpeed < 0.5f && moveSpeed > -0.5f ? 0 : moveSpeed);
+        }
+        Debug.Log(moveSpeed);
+        rb.MovePosition(rb.position + new Vector2(transform.up.x, transform.up.y) * moveSpeed * Time.fixedDeltaTime);
+
+        rb.MoveRotation(rb.rotation + rotSpeed * -rotation * Time.fixedDeltaTime);
     }
 }
