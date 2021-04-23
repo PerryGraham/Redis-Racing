24aea8205a09cd6c1cc07059f0d96aa4d2ae66e6
Graham Pinsent <graham.pinsent@gmail.com>
2021-04-23 14:56:13 -0700

    redis json response working

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,16 +1,19 @@
 require('dotenv').config();
-require("redis-json")
 var bodyParser = require('body-parser');
-const express = require('express')
-const app = express();
-const port = 80
+const 
+  rejson = require('redis-rejson');
+  redis = require('redis');
+  // json = require('json')
+  express = require('express')
+  app = express();
+  port = 80
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+rejson(redis);
+
 let
-  redis     = require('redis'),
-  /* Values are hard-coded for this example, it's usually best to bring these in via file or environment variable for production */
   client    = redis.createClient({
     port      : 12282,               // replace with your port
     host      : process.env.HOST,        // replace with your hostanme or IP address
@@ -20,14 +23,15 @@ let
 app.post("/updatepos", (req, res, next) => {
   var myjson = req.body;
   const data = {
-    xPos: myjson.xPos,
-    yPos: myjson.yPos,
-    zRot: myjson.zRot
+    "name": myjson.name,
+    "xPos": myjson.xPos,
+    "yPos": myjson.yPos,
+    "zRot": myjson.zRot,
+    "lastping": new Date()
   }
-  client.hset("playerpositions","Graham", JSON.stringify(data))
-  const returndata = client.hgetall("playerpositions", function (err, results) {
-    console.log(results)
-    res.send(results)
+  client.json_set("players",myjson.name, JSON.stringify(data))
+  const returndata = client.json_get("players", function (err, results) {
+    res.send(Object.values(JSON.parse(results)))
   })
 })
 
