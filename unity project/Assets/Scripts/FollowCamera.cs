f71f491d169123dc918a0ac2bcfeec1c339df1af
jpasqui <jeremypasquino@gmail.com>
2021-05-04 04:47:59 -0400

    Fixed lines between tiles and added camera rotation

--- unity project/Assets/Scripts/FollowCamera.cs
+++ unity project/Assets/Scripts/FollowCamera.cs
@@ -6,9 +6,11 @@ public class FollowCamera : MonoBehaviour
 {
     public Transform car;
     public Vector3 cameraOffset;
-    float smoothSpeed = 7.5f;
+    float positionSpeed = 7.5f;
+    float rotationSpeed = 7.5f;
     void LateUpdate() {
         if (!car) { return; }
-        transform.position = Vector3.Lerp(transform.position, car.transform.position + cameraOffset, smoothSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, car.transform.position + cameraOffset, positionSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, car.rotation, rotationSpeed * Time.deltaTime);
     }
 }
